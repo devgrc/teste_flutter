cea19d2d49d8b4c153c03aa3a803446aab661e4c
Antony Garcia <antonysenagarcia10@gmail.com>
2024-09-03 17:04:19 -0300

    atualizacao textFormField

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teste_flutter/features/login/login_page.dart';
 import 'package:teste_flutter/features/sign_up/sign_up_page.dart';
 
 class App extends StatelessWidget {
@@ -7,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SignUpPage(),
+      home: LoginPage(),
     );
   }
 }
