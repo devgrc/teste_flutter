ef8aa48fd7c1b7844c43a4bb9ec9cbf3cfd5e3e6
Antony Garcia <antonysenagarcia10@gmail.com>
2024-10-07 12:13:20 -0300

    feat: :sparkles: Adiciona parte da responsividade do app, para atender dispositivos de disferentes tamanhos e proporções.
    -Falta resolver a questao do width dos containers

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:teste_flutter/common/app_text_styles.dart'; // Certifique-se de ter esse arquivo criado com as devidas definições de estilos de texto.
+import 'package:teste_flutter/common/app_text_styles.dart';
+import 'package:teste_flutter/common/extensions/size.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +10,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double get TextScaleFactor => MediaQuery.of(context).size.width < 360.0 ? 0.7 : 1.0;
+  double get iconsize => MediaQuery.of(context).size.width < 360.0 ? 16.0 : 24.0;
   int _selectedIndex = 0;
 
   final List<Widget> _widgetOptions = <Widget>[
@@ -26,151 +29,169 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    Sizes.init(context);
+    final textScaleFactor = MediaQuery.textScaleFactorOf(context);
+
     return Scaffold(
-      body: Column(
-        children: [
-          // Seção de boas-vindas
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0),
-            child: Container(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 30.0,
-                        backgroundImage: AssetImage('assets/images/avatar.png'),
-                      ),
-                      SizedBox(width: 16.0),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Bem-vindo!',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Seção de boas-vindas
+            Padding(
+              padding: EdgeInsets.only(top: 40.h),
+              child: Container(
+                padding: EdgeInsets.all(16.h),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 30.0,
+                          backgroundImage: AssetImage('assets/images/avatar.png'),
+                        ),
+                        SizedBox(width: 16.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Bem-vindo!',
                               style: TextStyle(
-                                  fontSize: 20.0, fontWeight: FontWeight.bold)),
-                          Text('Nome do Usuário',
-                              style: TextStyle(fontSize: 16.0)),
-                        ],
+                                fontSize: 20.0 * textScaleFactor, 
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              'Nome do Usuário',
+                              style: TextStyle(fontSize: 16.0 * textScaleFactor),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.notifications),
+                      onPressed: () {
+                        // Lógica para abrir a tela de notificações
+                      },
+                      color: Colors.black,
+                      iconSize: 30.0,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 35.h),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20.h),
+                  width: 370.w,
+                  height: 111.h,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF003617),
+                    borderRadius: BorderRadius.circular(20.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 10,
+                        offset: const Offset(0, 4),
                       ),
                     ],
                   ),
-                  IconButton(
-                    icon: const Icon(Icons.notifications),
-                    onPressed: () {
-                      // Lógica para abrir a tela de notificações
-                    },
-                    color: Colors.black,
-                    iconSize: 30.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Saldo', style: AppTextStyles.KanitSaldo.copyWith(fontSize: 20 * textScaleFactor)),
+                      Text('R\$ 100,00', style: AppTextStyles.KodchasanValor.copyWith(fontSize: 16 * textScaleFactor)),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                SizedBox(height: 20.h),
+                Padding(
+                  padding: const EdgeInsets.only(left: 0.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Botão Receitas
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            print('Botão esquerdo pressionado!');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: Size(70.w, 100.h),
+                            backgroundColor: const Color(0xFFB8EFCB),
+                            elevation: 7.0,
+                            shadowColor: const Color(0xFF000000),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const SizedBox(
+                                height: 40.0,
+                                width: 52.0,
+                                child: Icon(Icons.arrow_upward,
+                                    color: Colors.black, size: 40.0),
+                              ),
+                              SizedBox(height: 4.0),
+                              Text('Receitas',
+                                  style: AppTextStyles.KantumruyDespRecei.copyWith(fontSize: 16 * textScaleFactor)),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20.w),
+                      const BotaoAdd(),
+                      SizedBox(width: 20.w),
+                      // Botão Despesas
+                      Expanded(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            print('Botão direito pressionado!');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: Size(70.w, 100.h),
+                            backgroundColor: const Color(0xFFB8EFCB),
+                            elevation: 7.0,
+                            shadowColor: const Color(0xFF000000),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const SizedBox(
+                                height: 40.0,
+                                width: 40.0,
+                                child: Icon(Icons.arrow_downward,
+                                    color: Colors.black, size: 40.0),
+                              ),
+                              const SizedBox(height: 4.0),
+                              Text('Despesas',
+                                  style: AppTextStyles.KantumruyDespRecei.copyWith(fontSize: 16 * textScaleFactor)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ),
-          const SizedBox(height: 35.0),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                width: 370.0,
-                height: 111.0,
-                decoration: BoxDecoration(
-                  color: const Color(0xFF003617),
-                  borderRadius: BorderRadius.circular(20.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 10,
-                      offset: const Offset(0, 4),
-                    ),
-                  ],
-                ),
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Saldo', style: AppTextStyles.KanitSaldo),
-                    Text('R\$ 100,00', style: AppTextStyles.KodchasanValor),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                  height:
-                      20.0), // Espaçamento entre o container e a linha de botões
-              Padding(
-                padding: const EdgeInsets.only(left: 33.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    // Primeiro botão menor à esquerda do botaoAdd
-                    ElevatedButton(
-                      onPressed: () {
-                        print('Botão esquerdo pressionado!');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(43.0, 100.0),
-                        backgroundColor: const Color(0xFFB8EFCB),
-                        elevation: 7.0,
-                        shadowColor: const Color(0xFF000000),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                      child: const Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.arrow_upward,
-                              color: Colors.black, size: 40.0),
-                          SizedBox(height: 4.0),
-                          Text('Receitas',
-                              style: AppTextStyles.KantumruyDespRecei),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(width: 14.0), // Espaçamento entre os botões
-                    // Botão principal (botaoAdd)
-                    const botaoAdd(),
-                    const SizedBox(width: 14.0),
-                    // Segundo botão menor à direita do botaoAdd
-                    ElevatedButton(
-                      onPressed: () {
-                        print('Botão direito pressionado!');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(43.0, 100.0),
-                        backgroundColor: const Color(0xFFB8EFCB),
-                        elevation: 7.0,
-                        shadowColor: const Color(0xFF000000),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                      child: const Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.arrow_downward,
-                              color: Colors.black, size: 40.0),
-                          SizedBox(height: 4.0),
-                          Text('Despesas',
-                              style: AppTextStyles.KantumruyDespRecei),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 35.0),
-          // Corpo da HomePage
-          Expanded(
-            child: Center(
+            SizedBox(height: 35.h),
+            // Corpo da HomePage
+            Center(
               child: _widgetOptions[_selectedIndex],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       bottomNavigationBar: BottomAppBar(
         shape: const CircularNotchedRectangle(),
@@ -228,8 +249,8 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class botaoAdd extends StatelessWidget {
-  const botaoAdd({
+class BotaoAdd extends StatelessWidget {
+  const BotaoAdd({
     super.key,
   });
 
@@ -240,12 +261,12 @@ class botaoAdd extends StatelessWidget {
         print('Botão pressionado!');
       },
       style: ElevatedButton.styleFrom(
-        minimumSize: const Size(123.0, 125.0),
-        backgroundColor: const Color(0xFFB8EFCB), // Cor de fundo
+        minimumSize: Size(123.w, 125.h),
+        backgroundColor: const Color(0xFFB8EFCB),
         elevation: 7.0,
         shadowColor: const Color(0xFF000000),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0), // Cantos arredondados
+          borderRadius: BorderRadius.circular(10.0),
         ),
       ),
       child: Container(
@@ -253,16 +274,13 @@ class botaoAdd extends StatelessWidget {
         width: 75.0,
         height: 68.0,
         decoration: const BoxDecoration(
-          color: Color(0xFF003617), // Cor do quadrado interno
-          shape: BoxShape.rectangle,
-          borderRadius: BorderRadius.all(
-            Radius.circular(10.0),
-          ),
+          color: Color(0xFF003617),
+          shape: BoxShape.circle,
         ),
         child: const Align(
           alignment: Alignment.center,
           child: Icon(
-            Icons.add_to_photos_rounded, // Ícone de mais
+            Icons.add_to_photos_rounded,
             color: Colors.white,
             size: 40.0,
           ),
@@ -270,10 +288,4 @@ class botaoAdd extends StatelessWidget {
       ),
     );
   }
-}
-
-void main() {
-  runApp(const MaterialApp(
-    home: HomePage(),
-  ));
 }
