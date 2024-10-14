9e5e5e9bffa1500fb8c6830f2ce4eccc85369fa8
Antony Garcia <antonysenagarcia10@gmail.com>
2024-10-14 13:48:57 -0300

    feat: :sparkles: Adiciona novos modelos de fontes ao projeto

--- lib/common/app_text_styles.dart
+++ lib/common/app_text_styles.dart
@@ -4,46 +4,68 @@ class AppTextStyles {
   AppTextStyles._();
 
   static const TextStyle KodchasanOnBoarding = TextStyle(
-    fontFamily: 'Kodchasan',
-    fontSize: 50.0,
-    fontWeight: FontWeight.normal,
-    color: Color.fromARGB(255, 255, 255, 255)
-  );
+      fontFamily: 'Kodchasan',
+      fontSize: 50.0,
+      fontWeight: FontWeight.normal,
+      color: Color.fromARGB(255, 255, 255, 255));
   static const TextStyle KodchasanLogin = TextStyle(
-    fontFamily: 'Kodchasan',
-    fontSize: 17.0,
-    fontWeight: FontWeight.normal,
-    color: Color.fromARGB(255, 255, 255, 255)
-  );
+      fontFamily: 'Kodchasan',
+      fontSize: 17.0,
+      fontWeight: FontWeight.normal,
+      color: Color.fromARGB(255, 255, 255, 255));
   static const TextStyle KantumruyButton = TextStyle(
-    fontFamily: 'Kantumruy',
-    fontSize: 28.0,
-    fontWeight: FontWeight.bold,
-    color: Colors.black
-  );
+      fontFamily: 'Kantumruy',
+      fontSize: 28.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.black);
   static const TextStyle KantumLogin1 = TextStyle(
-    fontFamily: 'Kodchasan',
-    fontSize: 40.0,
-    fontWeight: FontWeight.bold,
-    color: Color.fromARGB(255, 255, 255, 255)
-  );
+      fontFamily: 'Kodchasan',
+      fontSize: 40.0,
+      fontWeight: FontWeight.bold,
+      color: Color.fromARGB(255, 255, 255, 255));
   static const TextStyle KantumLogin2 = TextStyle(
-    fontFamily: 'Kantumruy',
-    fontSize: 20.0,
-    fontWeight: FontWeight.w200,
-    color: Colors.white
-  );
+      fontFamily: 'Kantumruy',
+      fontSize: 20.0,
+      fontWeight: FontWeight.w200,
+      color: Colors.white);
   static const TextStyle KanitSaldo = TextStyle(
-    fontFamily: 'Kanit',
-    fontSize: 17.0,
-    fontWeight: FontWeight.w300,
-    color: Colors.white
-  );
+      fontFamily: 'Kanit',
+      fontSize: 17.0,
+      fontWeight: FontWeight.w300,
+      color: Colors.white);
   static const TextStyle KodchasanValor = TextStyle(
-    fontFamily: 'Kodchasan',
-    fontSize: 26.0,
-    fontWeight: FontWeight.w600,
-    color: Colors.white
-  );
-  
+      fontFamily: 'Kodchasan',
+      fontSize: 26.0,
+      fontWeight: FontWeight.w600,
+      color: Colors.white);
+  static const TextStyle KantumruyDespRecei = TextStyle(
+      fontFamily: 'Kantumruy',
+      fontSize: 12.0,
+      fontWeight: FontWeight.w600,
+      color: Color.fromARGB(255, 0, 0, 0));
+  static const TextStyle HistoricoRecente = TextStyle(
+      fontFamily: 'Montserrat',
+      fontSize: 14.0,
+      fontWeight: FontWeight.w500,
+      color: Color.fromARGB(255, 0, 0, 0));
+  static const TextStyle TituloDesRec = TextStyle(
+      fontFamily: 'Montserrat',
+      fontSize: 14.0,
+      fontWeight: FontWeight.w600,
+      color: Color.fromARGB(255, 0, 0, 0));
+  static const TextStyle SubCategoria = TextStyle(
+      fontFamily: 'Montserrat',
+      fontSize: 12.0,
+      fontWeight: FontWeight.w400,
+      color: Color.fromARGB(255, 0, 0, 0));
+  static const TextStyle Valor = TextStyle(
+      fontFamily: 'Kanit',
+      fontSize: 18.0,
+      fontWeight: FontWeight.w500,
+      color: Color.fromARGB(255, 0, 0, 0));
+  static const TextStyle Date = TextStyle(
+      fontFamily: 'MontserratAlternates',
+      fontSize: 10.0,
+      fontWeight: FontWeight.w500,
+      color: Color.fromARGB(255, 0, 0, 0));
 }
