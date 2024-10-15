147cfad2095194e84e08a227509730a55b0ed88f
Antony Garcia <antonysenagarcia10@gmail.com>
2024-10-15 13:20:49 -0300

    feat: routes no app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:teste_flutter/common/constants/routes.dart';
+import 'package:teste_flutter/features/home/home_page.dart';
 import 'package:teste_flutter/features/login/login_page.dart';
 import 'package:teste_flutter/features/onbording/onbording_page.dart';
 import 'package:teste_flutter/features/sign_up/sign_up_page.dart';
@@ -11,12 +12,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: NamedRoutes.splash,
+      initialRoute: NamedRoutes.signUp,
       routes: {
         NamedRoutes.initial: (context) => const OnboardngPage(),
         NamedRoutes.splash: (context) => const SplashPage(),
         NamedRoutes.signUp: (context) => const SignUpPage(),
         NamedRoutes.login: (context) => const LoginPage(),
+        NamedRoutes.home: (context) => const HomePage(),
       },
     );
   }
