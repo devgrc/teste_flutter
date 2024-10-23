5096de067dd99799b4255b305e93fc4ce2ac2b08
Antony Garcia <antonysenagarcia10@gmail.com>
2024-10-23 11:58:00 -0300

    feat: :sparkles: Adiciona funcionalidade de autenticação do usuário, controlando e se comunicando com a login_page

--- lib/features/login/login_controller.dart
+++ lib/features/login/login_controller.dart
@@ -1,26 +1,32 @@
 import 'dart:async';
+import 'dart:ui';
 import 'login_state.dart';
 
 class LoginController {
-  final StreamController<SignInState> _stateController = StreamController<SignInState>();
+  final _stateController = StreamController<SignInState>();
 
   Stream<SignInState> get state => _stateController.stream;
 
-  void dispose() {
-    _stateController.close();
-  }
-
-  void submitForm(String email, String password, Function onSuccess) {
+  void submitForm(String email, String password, VoidCallback onSuccess) {
     _stateController.add(SignInStateLoading());
 
-    // Simulação de uma requisição de login
-    Future.delayed(const Duration(seconds: 1), () {
-      if (email.isNotEmpty && password.length >= 6) {
-        _stateController.add(SignInStateSuccess());
-        onSuccess(); // Chama a função de sucesso
-      } else {
-        _stateController.add(SignInStateError("Credenciais inválidas."));
+    // Simulação de uma chamada de API
+    Future.delayed(const Duration(seconds: 2), () {
+      try {
+        // Lógica de autenticação
+        if (email.isNotEmpty && password.isNotEmpty) {
+          _stateController.add(SignInStateSuccess());
+          onSuccess();
+        } else {
+          _stateController.add(SignInStateError('Credenciais inválidas'));
+        }
+      } catch (e) {
+        _stateController.add(SignInStateError('Erro ao fazer login'));
       }
     });
+  }
+
+  void dispose() {
+    _stateController.close();
   }
 }
