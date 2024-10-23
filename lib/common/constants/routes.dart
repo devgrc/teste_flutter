061c302accb4302aa11ea8344565da8cdd5ec930
Antony Garcia <antonysenagarcia10@gmail.com>
2024-10-23 11:46:20 -0300

    feat: Adiciona rotas addSaldo e add_recei_desp

--- lib/common/constants/routes.dart
+++ lib/common/constants/routes.dart
@@ -7,5 +7,7 @@ class NamedRoutes {
   static const String login = "login";
   static const String saldo = "saldo";
   static const String home = "home";
+  static const String addSaldo = "addSaldo";
+  static const String add_recei_desp = "add_recei_desp";
   
 }
