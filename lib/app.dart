5aabd7aba34e68bde7f4ea870166c94dac019ba4
Antony Garcia <antonysenagarcia10@gmail.com>
2024-10-23 12:03:38 -0300

    feat: Adiciona o change notifier provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,27 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:teste_flutter/common/constants/routes.dart';
-import 'package:teste_flutter/features/home/home_page.dart';
-import 'package:teste_flutter/features/login/login_page.dart';
 import 'package:teste_flutter/features/onbording/onbording_page.dart';
-import 'package:teste_flutter/features/saldo/saldo_page.dart';
 import 'package:teste_flutter/features/sign_up/sign_up_page.dart';
-import 'package:teste_flutter/features/splash/splash_page.dart';
+import 'features/home/home_controller.dart';
+import 'features/home/home_page.dart';
+import 'features/login/login_page.dart';
+import 'features/saldo/add_saldo_page.dart';
+import 'features/splash/splash_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: NamedRoutes.saldo,
-      routes: {
-        NamedRoutes.initial: (context) => const OnboardngPage(),
-        NamedRoutes.splash: (context) => const SplashPage(),
-        NamedRoutes.signUp: (context) => const SignUpPage(),
-        NamedRoutes.login: (context) => const LoginPage(),
-        NamedRoutes.saldo: (context) => const SaldoPage(),
-        NamedRoutes.home: (context) => const HomePage(),
-      },
+    return ChangeNotifierProvider(
+      create: (_) => HomeController(),
+      child: MaterialApp(
+        initialRoute: NamedRoutes.addSaldo,
+        routes: {
+          NamedRoutes.signUp: (context) => const SignUpPage(),
+          NamedRoutes.login: (context) => const LoginPage(),
+          NamedRoutes.addSaldo: (context) => AddSaldoPage(homeController: context.read<HomeController>()),
+          NamedRoutes.home: (context) => HomePage(),
+          NamedRoutes.initial: (context) => const OnboardngPage(),
+          NamedRoutes.splash: (context) => const SplashPage(),
+        },
+      ),
     );
   }
+}
+
+void main() {
+  runApp(const App());
 }
