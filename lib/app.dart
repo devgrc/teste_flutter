edf51d51a60120d000d0e80c9ac47ebc9497a520
Antony Garcia <antonysenagarcia10@gmail.com>
2024-10-28 13:14:38 -0300

    feat: :sparkles: Adiciona o multiprovider e novas rotas

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:teste_flutter/common/constants/routes.dart';
+import 'package:teste_flutter/features/chatbot/chat_bot_page.dart';
 import 'package:teste_flutter/features/onbording/onbording_page.dart';
 import 'package:teste_flutter/features/sign_up/sign_up_page.dart';
+import 'package:teste_flutter/features/splash/splash_page.dart';
+import 'package:teste_flutter/features/transaction/add_transaction_view.dart';
 import 'features/home/home_controller.dart';
 import 'features/home/home_page.dart';
 import 'features/login/login_page.dart';
 import 'features/saldo/add_saldo_page.dart';
-import 'features/splash/splash_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => HomeController(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => HomeController()),
+      ],
       child: MaterialApp(
-        initialRoute: NamedRoutes.addSaldo,
+        initialRoute: NamedRoutes.home,
         routes: {
           NamedRoutes.signUp: (context) => const SignUpPage(),
           NamedRoutes.login: (context) => const LoginPage(),
@@ -25,6 +29,8 @@ class App extends StatelessWidget {
           NamedRoutes.home: (context) => HomePage(),
           NamedRoutes.initial: (context) => const OnboardngPage(),
           NamedRoutes.splash: (context) => const SplashPage(),
+          NamedRoutes.chat: (context) => const ChatBotPage(),
+          NamedRoutes.transaction: (context) => AddTransactionPage(), // Adicionando a rota para adicionar transação
         },
       ),
     );
