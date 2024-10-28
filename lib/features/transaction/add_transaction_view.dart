bf272801ca2386f9c53fc1d05d40e074dc1136e5
Antony Garcia <antonysenagarcia10@gmail.com>
2024-10-28 16:40:58 -0300

    feat: :sparkles: Adiciona campo de data e adiciona funcionalidade de adicionar ícones

--- lib/features/transaction/add_transaction_view.dart
+++ lib/features/transaction/add_transaction_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:teste_flutter/features/home/home_controller.dart';
 
@@ -13,6 +15,54 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
   final _valorController = TextEditingController();
   final _categoriaController = TextEditingController();
   String _tipo = 'Receita';
+  DateTime _data = DateTime.now();
+  IconData? _selectedIcon; // Campo para armazenar o ícone selecionado
+
+  // Método para exibir um seletor de ícones
+  void _selectIcon(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Escolha um Ícone'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // Lista de ícones disponíveis
+                ...[ 
+                  Icons.attach_money,
+                  Icons.arrow_upward,
+                  Icons.arrow_downward,
+                  Icons.shopping_cart,
+                  Icons.paid,
+                  // Adicione mais ícones conforme necessário
+                ].map((icon) {
+                  return IconButton(
+                    icon: Icon(icon),
+                    onPressed: () {
+                      setState(() {
+                        _selectedIcon = icon; // Armazena o ícone selecionado
+                      });
+                      Navigator.pop(context);
+                    },
+                  );
+                }).toList(),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +90,9 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                 controller: _valorController,
                 decoration: InputDecoration(labelText: 'Valor'),
                 keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')),
+                ],
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Por favor, insira o valor';
@@ -72,6 +125,35 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                 }).toList(),
                 decoration: InputDecoration(labelText: 'Tipo'),
               ),
+              // Exibição do ícone selecionado e botão para escolher um novo
+              GestureDetector(
+                onTap: () => _selectIcon(context),
+                child: Row(
+                  children: [
+                    Icon(_selectedIcon ?? Icons.monetization_on), // Exibe o ícone selecionado ou um ícone padrão
+                    SizedBox(width: 8),
+                    Text('Escolha um ícone'),
+                  ],
+                ),
+              ),
+              TextFormField(
+                readOnly: true,
+                controller: TextEditingController(text: DateFormat('dd/MM/yyyy').format(_data)),
+                decoration: InputDecoration(labelText: 'Data'),
+                onTap: () async {
+                  final selectedDate = await showDatePicker(
+                    context: context,
+                    initialDate: _data,
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2100),
+                  );
+                  if (selectedDate != null && selectedDate != _data) {
+                    setState(() {
+                      _data = selectedDate;
+                    });
+                  }
+                },
+              ),
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: () {
@@ -80,7 +162,7 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                     final valor = double.parse(_valorController.text);
                     final categoria = _categoriaController.text;
                     final tipo = _tipo;
-                    final data = DateTime.now();
+                    final data = _data;
                     final isCredit = tipo == 'Receita';
 
                     final transaction = ReceitaDespesa(
@@ -90,6 +172,7 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                       categoria: categoria,
                       data: data,
                       isCredit: isCredit,
+                      icon: _selectedIcon ?? Icons.monetization_on, // Armazenar o ícone selecionado ou um padrão
                     );
 
                     context.read<HomeController>().addReceitaDespesa(transaction);
