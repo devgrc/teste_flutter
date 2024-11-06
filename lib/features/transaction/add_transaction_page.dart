fae3fbcd346c12e575ba8a0634503180d7874d52
Antony Garcia <antonysenagarcia10@gmail.com>
2024-11-06 16:46:27 -0300

    feat: :sparkles: Mudança na pagina de transação ícones, mudança de estilo...

--- lib/features/transaction/add_transaction_page.dart
+++ lib/features/transaction/add_transaction_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import 'package:teste_flutter/common/app_text_styles.dart';
 import 'package:teste_flutter/features/home/home_controller.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart'; // Importando o FontAwesome
 
 class AddTransactionPage extends StatefulWidget {
   @override
@@ -16,36 +19,85 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
   final _categoriaController = TextEditingController();
   String _tipo = 'Receita';
   DateTime _data = DateTime.now();
-  IconData? _selectedIcon; // Campo para armazenar o ícone selecionado
+  IconData? _selectedIcon =
+      FontAwesomeIcons.dollarSign; // Definir um ícone padrão
 
-  // Método para exibir um seletor de ícones
+  // Função de escolha do ícone
   void _selectIcon(BuildContext context) {
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text('Escolha um Ícone'),
-          content: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+          title: const Text('Escolha um Ícone', textAlign: TextAlign.center),
+          content: SizedBox(
+            width: 300, // Limitar a largura do diálogo
+            height: 350, // Limitar a altura
+            child: GridView.count(
+              crossAxisCount: 4, // Número de colunas no grid
+              crossAxisSpacing: 12, // Espaçamento horizontal entre os ícones
+              mainAxisSpacing: 12, // Espaçamento vertical entre os ícones
               children: [
-                // Lista de ícones disponíveis
-                ...[ 
-                  Icons.attach_money,
-                  Icons.arrow_upward,
-                  Icons.arrow_downward,
-                  Icons.shopping_cart,
-                  Icons.paid,
-                  // Adicione mais ícones conforme necessário
+                ...[
+                   Ionicons.cash, // Dinheiro
+                  Ionicons.trending_up, // Tendência de Alta
+                  Ionicons.trending_down, // Tendência de Baixa
+                  Ionicons.swap_horizontal, // Transferência Horizontal
+                  Ionicons.swap_vertical, // Transferência Vertical
+                  Ionicons.receipt, // Recibo
+                  Ionicons.wallet_outline, // Carteira Contorno
+                  Ionicons.calculator, // Calculadora
+                  Ionicons.cube, // Cubo (Investimento)
+                  Ionicons.contract, // Contrato
+                  Ionicons.wallet, // Cartera
+                  Ionicons.card, // Cartão
+                  Ionicons.cash, // Dinheiro
+                  Ionicons.pizza, // Pizza
+                  Ionicons.fast_food, // Fast Food
+                  Ionicons.restaurant, // Restaurante
+                  Ionicons.car, // Carro
+                  Ionicons.bicycle, // Bicicleta
+                  Ionicons.bus, // Ônibus
+                  Ionicons.airplane, // Avião
+                  Ionicons.train, // Trem
+                  Ionicons.heart, // Coração
+                  Ionicons.medical, // Médico
+                  Ionicons.bed, // Cama
+                  Ionicons.game_controller, // Controle de Jogo
+                  Ionicons.gift, // Presente
+                  Ionicons.camera, // Câmera
+                  Ionicons.cart, // Carrinho de Compras
+                  Ionicons.home, // Casa
+                  Ionicons.phone_portrait, // Telefone
+                  Ionicons.laptop, // Laptop
+                  Ionicons.book, // Livro
+                  Ionicons.headset, // Fone de Ouvido
+                  Ionicons.wine, // Vinho
+                  Ionicons.water, // Água
+                  Ionicons.cog, // Engrenagem
+                  Ionicons.briefcase, // Pasta
+                  Ionicons.trophy, // Troféu
+                  Ionicons.school, // Escola
+                  Ionicons.earth, // Terra
+                  Ionicons.flag, // Bandeira
+                  Ionicons.ice_cream, // Sorvete
+                  Ionicons.moon, // Lua
+                  Ionicons.sunny, // Sol
+                  Ionicons.leaf, // Folha
+                  Ionicons.flower, // Flor
+                 
                 ].map((icon) {
-                  return IconButton(
-                    icon: Icon(icon),
-                    onPressed: () {
+                  return GestureDetector(
+                    onTap: () {
                       setState(() {
-                        _selectedIcon = icon; // Armazena o ícone selecionado
+                        _selectedIcon = icon;
                       });
                       Navigator.pop(context);
                     },
+                    child: Icon(
+                      icon,
+                      size: 30,
+                      color: _selectedIcon == icon ? Colors.blue : Colors.black,
+                    ),
                   );
                 }).toList(),
               ],
@@ -56,7 +108,7 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
               onPressed: () {
                 Navigator.pop(context);
               },
-              child: Text('Cancelar'),
+              child: const Text('Cancelar'),
             ),
           ],
         );
@@ -67,18 +119,48 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF003617), // Cor do fundo
       appBar: AppBar(
-        title: Text('Adicionar Transação'),
+        title: const Text(
+          'Adicionar Transação',
+          style: AppTextStyles.KodchasanAddTandaction,
+        ),
+        centerTitle: true, // Título centralizado
+        backgroundColor: const Color(0xFF003617), // Cor de fundo da AppBar
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back,
+              color: Colors.white), // Ícone de voltar branco
+          onPressed: () {
+            Navigator.pop(context); // Voltar para a tela anterior
+          },
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: Column(
+          child: ListView(
             children: [
+              // Nome Field
               TextFormField(
                 controller: _nomeController,
-                decoration: InputDecoration(labelText: 'Nome'),
+                style: const TextStyle(color: Colors.white),
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: 'Nome',
+                  labelStyle: const TextStyle(
+                      color: Color.fromARGB(209, 255, 255, 255)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  hintText: 'Insira o nome',
+                  hintStyle: const TextStyle(
+                      color: Color.fromARGB(103, 255, 255, 255)),
+                  prefixIcon: const Icon(Icons.person, color: Colors.white),
+                ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Por favor, insira o nome';
@@ -86,13 +168,33 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                   return null;
                 },
               ),
+              const SizedBox(height: 16),
+
+              // Valor Field
               TextFormField(
                 controller: _valorController,
-                decoration: InputDecoration(labelText: 'Valor'),
+                style: const TextStyle(color: Colors.white),
+                textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 inputFormatters: [
                   FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')),
                 ],
+                decoration: InputDecoration(
+                  labelText: 'Valor',
+                  labelStyle: const TextStyle(
+                      color: Color.fromARGB(209, 255, 255, 255)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  hintText: 'Insira o valor',
+                  hintStyle: const TextStyle(
+                      color: Color.fromARGB(103, 255, 255, 255)),
+                  prefixIcon:
+                      const Icon(Icons.attach_money, color: Colors.white),
+                ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Por favor, insira o valor';
@@ -100,9 +202,28 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                   return null;
                 },
               ),
+              const SizedBox(height: 16),
+
+              // Categoria Field
               TextFormField(
                 controller: _categoriaController,
-                decoration: InputDecoration(labelText: 'Categoria'),
+                style: const TextStyle(color: Colors.white),
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: 'Categoria',
+                  labelStyle: const TextStyle(
+                      color: Color.fromARGB(209, 255, 255, 255)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  hintText: 'Insira a categoria',
+                  hintStyle: const TextStyle(
+                      color: Color.fromARGB(103, 255, 255, 255)),
+                  prefixIcon: const Icon(Icons.category, color: Colors.white),
+                ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Por favor, insira a categoria';
@@ -110,6 +231,9 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                   return null;
                 },
               ),
+              const SizedBox(height: 16),
+
+              // Tipo Dropdown
               DropdownButtonFormField<String>(
                 value: _tipo,
                 onChanged: (value) {
@@ -120,26 +244,72 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                 items: ['Receita', 'Despesa'].map((String tipo) {
                   return DropdownMenuItem<String>(
                     value: tipo,
-                    child: Text(tipo),
+                    child: Text(
+                      tipo,
+                      style: const TextStyle(color: Colors.black),
+                    ),
                   );
                 }).toList(),
-                decoration: InputDecoration(labelText: 'Tipo'),
+                decoration: InputDecoration(
+                  labelText: 'Tipo',
+                  labelStyle: const TextStyle(
+                      color: Color.fromARGB(209, 255, 255, 255)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+                style: const TextStyle(color: Colors.white),
               ),
-              // Exibição do ícone selecionado e botão para escolher um novo
+              const SizedBox(height: 16),
+
+              // Icon Selection
               GestureDetector(
                 onTap: () => _selectIcon(context),
                 child: Row(
                   children: [
-                    Icon(_selectedIcon ?? Icons.monetization_on), // Exibe o ícone selecionado ou um ícone padrão
-                    SizedBox(width: 8),
-                    Text('Escolha um ícone'),
+                    Icon(
+                      _selectedIcon ??
+                          FontAwesomeIcons
+                              .dollarSign, // Exibindo o ícone selecionado
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(width: 8),
+                    const Text(
+                      'Escolha um ícone',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ],
                 ),
               ),
+              const SizedBox(height: 16),
+
+              // Data Picker Field
               TextFormField(
                 readOnly: true,
-                controller: TextEditingController(text: DateFormat('dd/MM/yyyy').format(_data)),
-                decoration: InputDecoration(labelText: 'Data'),
+                controller: TextEditingController(
+                  text: DateFormat('dd/MM/yyyy').format(_data),
+                ),
+                style: const TextStyle(color: Colors.white),
+                textInputAction: TextInputAction.done,
+                decoration: InputDecoration(
+                  labelText: 'Data',
+                  labelStyle: const TextStyle(
+                      color: Color.fromARGB(209, 255, 255, 255)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  hintStyle: const TextStyle(
+                      color: Color.fromARGB(103, 255, 255, 255)),
+                  prefixIcon:
+                      const Icon(Icons.calendar_today, color: Colors.white),
+                ),
                 onTap: () async {
                   final selectedDate = await showDatePicker(
                     context: context,
@@ -154,32 +324,53 @@ class _AddTransactionPageState extends State<AddTransactionPage> {
                   }
                 },
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    final nome = _nomeController.text;
-                    final valor = double.parse(_valorController.text);
-                    final categoria = _categoriaController.text;
-                    final tipo = _tipo;
-                    final data = _data;
-                    final isCredit = tipo == 'Receita';
+              const SizedBox(height: 30),
 
-                    final transaction = ReceitaDespesa(
-                      nome: nome,
-                      valor: valor,
-                      tipo: tipo,
-                      categoria: categoria,
-                      data: data,
-                      isCredit: isCredit,
-                      icon: _selectedIcon ?? Icons.monetization_on, // Armazenar o ícone selecionado ou um padrão
-                    );
+              // Submit Button
+              Center(
+                child: SizedBox(
+                  width: 335.32,
+                  height: 59.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        final nome = _nomeController.text;
+                        final valor = double.parse(_valorController.text);
+                        final categoria = _categoriaController.text;
+                        final tipo = _tipo;
+                        final data = _data;
+                        final isCredit = tipo == 'Receita';
 
-                    context.read<HomeController>().addReceitaDespesa(transaction);
-                    Navigator.pop(context);
-                  }
-                },
-                child: Text('Adicionar Transação'),
+                        final transaction = ReceitaDespesa(
+                          nome: nome,
+                          valor: valor,
+                          tipo: tipo,
+                          categoria: categoria,
+                          data: data,
+                          isCredit: isCredit,
+                          icon: _selectedIcon ?? FontAwesomeIcons.dollarSign,
+                        );
+
+                        context
+                            .read<HomeController>()
+                            .addReceitaDespesa(transaction);
+                        Navigator.pop(context);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      elevation: 6,
+                      shadowColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(36),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                    ),
+                    child: const Text(
+                      'Adicionar Transação',
+                      style: AppTextStyles.KantumAddTandaction,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
