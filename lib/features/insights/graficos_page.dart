dcf37c8aa6c95bcb435711cd28457b90ee5a6d8c
Antony Garcia <antonysenagarcia10@gmail.com>
2024-11-07 16:38:08 -0300

    feat: :sparkles: Nova página de gráficos

---
+++ lib/features/insights/graficos_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GraficosPage extends StatelessWidget {
+  const GraficosPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
