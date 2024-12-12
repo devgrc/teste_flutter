8188117bc91a39f644adab1cbb58d6c12ae10ba3
Antony Garcia <antonysenagarcia10@gmail.com>
2024-12-12 09:46:26 -0300

    feat: Adiciona imports da categorias_page e graficos_page

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:teste_flutter/common/constants/routes.dart';
 import 'package:teste_flutter/features/categorias/categorias_page.dart';
 import 'package:teste_flutter/features/chatbot/chat_bot_page.dart';
+import 'package:teste_flutter/features/insights/graficos_page.dart';
 import 'package:teste_flutter/features/onbording/onbording_page.dart';
 import 'package:teste_flutter/features/perfil/perfil_page.dart';
 import 'package:teste_flutter/features/sign_up/sign_up_page.dart';
@@ -12,6 +13,7 @@ import 'features/home/home_controller.dart';
 import 'features/home/home_page.dart';
 import 'features/login/login_page.dart';
 import 'features/saldo/add_saldo_page.dart';
+import 'features/categorias/categorias_state.dart'; // Importe o arquivo do provider
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -21,6 +23,7 @@ class App extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => HomeController()),
+        ChangeNotifierProvider(create: (_) => CategoryProvider()), // Adicione o CategoryProvider aqui
       ],
       child: MaterialApp(
         initialRoute: NamedRoutes.home,
@@ -33,9 +36,9 @@ class App extends StatelessWidget {
           NamedRoutes.splash: (context) => const SplashPage(),
           NamedRoutes.chat: (context) => const ChatBotPage(),
           NamedRoutes.transaction: (context) => AddTransactionPage(), // Adicionando a rota para adicionar transação
-          NamedRoutes.chat: (context) => ChatBotPage(), // Adicionando a rota para adicionar transação
           NamedRoutes.perfil: (context) => PerfilPage(), // Adicionando a rota para adicionar transação
-          NamedRoutes.categorias: (context) => CategoriasPage(), 
+          NamedRoutes.categorias: (context) => CategoriasPage(),
+          NamedRoutes.graficos: (context) => GraficosPage(),
         },
       ),
     );
