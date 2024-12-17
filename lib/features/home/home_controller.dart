8c6565d15b1c15f4fdac9dccdf85585d7ed25e85
Antony Garcia <antonysenagarcia10@gmail.com>
2024-12-17 02:37:15 -0300

    feat: :sparkles: adiciona saldo inicial ao histórico de transações e atualiza barra de progresso

--- lib/features/home/home_controller.dart
+++ lib/features/home/home_controller.dart
@@ -28,7 +28,7 @@ class HomeState {
 class HomeController extends ChangeNotifier {
   double _balance = 0.0;  // Saldo atual
   double _initialBalance = 0.0; // Saldo inicial
-  double _progressBarWidth = 350; // Largura da barra de progresso
+  double _progressBarWidth = 0.0; // Largura da barra de progresso inicializada em zero
   List<ReceitaDespesa> _historico = [];
   final HomeState _state = HomeState();
 
@@ -45,12 +45,14 @@ class HomeController extends ChangeNotifier {
   void setInitialBalance(double initialBalance) {
     _initialBalance = initialBalance; // Salva o saldo inicial
     _balance = initialBalance; // O saldo atual é igual ao saldo inicial
-    _progressBarWidth = 350; // A barra de progresso começa cheia
+    _addInitialBalanceToHistorico(initialBalance); // Adiciona o saldo inicial ao histórico
+    _updateProgressBarWidth(); // Atualiza a barra de progresso com base no saldo inicial
     notifyListeners(); // Notifica os listeners para atualizar a UI
   }
 
   void updateBalance(double newBalance) {
     _balance = newBalance;
+    _updateProgressBarWidth();
     notifyListeners();
   }
 
@@ -64,20 +66,11 @@ class HomeController extends ChangeNotifier {
 
     if (item.tipo == 'Receita') {
       _balance += item.valor; // Incrementa o saldo com a receita
-
-      // Calcular a proporção do saldo em relação ao saldo inicial
-      double proportion = _balance / _initialBalance;
-
-      // Ajusta a largura da barra de progresso
-      _progressBarWidth = (proportion > 1) ? 350 : 350 * proportion;
     } else if (item.tipo == 'Despesa') {
       _balance -= item.valor; // Subtrai a despesa do saldo
-
-      // Ajuste da barra de progresso com base no saldo restante
-      double proportion = (_balance < 0) ? 0 : (_balance / (_balance + item.valor)).abs();
-      _progressBarWidth = 350 * proportion;
     }
 
+    _updateProgressBarWidth();
     notifyListeners();
   }
 
@@ -92,6 +85,7 @@ class HomeController extends ChangeNotifier {
       _balance += item.valor; // Incrementa o saldo com a despesa
     }
 
+    _updateProgressBarWidth();
     notifyListeners();
   }
 
@@ -153,6 +147,39 @@ class HomeController extends ChangeNotifier {
           item.data == DateTime.parse(vencimento['data']));
     }
 
+    _updateProgressBarWidth();
+    notifyListeners();
+  }
+
+  // Método para adicionar o saldo inicial ao histórico
+  void _addInitialBalanceToHistorico(double initialBalance) {
+    ReceitaDespesa initialBalanceTransaction = ReceitaDespesa(
+      nome: 'Saldo Inicial',
+      valor: initialBalance,
+      tipo: 'Receita',
+      categoria: 'Saldo Inicial',
+      data: DateTime.now(),
+      isCredit: true,
+      icon: Icons.attach_money,
+    );
+    _historico.add(initialBalanceTransaction);
+  }
+
+  // Método para atualizar a largura da barra de progresso com base no saldo atualizado
+  void _updateProgressBarWidth() {
+    if (_initialBalance == 0) {
+      _progressBarWidth = _balance > 0 ? 350 : 0;
+    } else {
+      double proportion = _balance / _initialBalance;
+      if (proportion <= 0) {
+        _progressBarWidth = 0;
+      } else if (proportion > 1) {
+        _progressBarWidth = 350;
+      } else {
+        _progressBarWidth = 350 * proportion;
+      }
+    }
+    print('ProgressBarWidth atualizado: $_progressBarWidth');
     notifyListeners();
   }
 }
